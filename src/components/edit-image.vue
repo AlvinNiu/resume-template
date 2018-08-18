ec23fa9382570efd0121e64c1bf8e8d9085bccfe
DESKTOP-G6GIG88\Alvin <912806786@qq.com>
2018-08-18 15:42:47 +0800

    删除无用图片

--- src/components/edit-image.vue
+++ src/components/edit-image.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="edit-image" :style="{ width: width + 'px', height: height + 'px'}">
-        <img :src="imgSrc" alt="image" :class="{ circle: isCircle }">
-        <input type="file" accept="image/gif,image/jpeg,image/jpg,image/png" @change="changeImage">
-    </div>
+  <div class="edit-image" :style="{ width: width + 'px', height: height + 'px'}">
+    <img :src="imgSrc" alt="image" :class="{ circle: isCircle }">
+    <input type="file" accept="image/gif,image/jpeg,image/jpg,image/png" @change="changeImage">
+  </div>
 </template>
 <script>
 export default {
